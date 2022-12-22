c5f569b54c06b16f8b5994f84b48dc41b541e486
MohsenSami <mohsensami88@gmail.com>
2022-12-22 21:09:03 +0330

    make password Noneditable

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from accounts.models import User
+from django.contrib.auth.admin import UserAdmin
 
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+
+admin.site.register(User, CustomUserAdmin)
