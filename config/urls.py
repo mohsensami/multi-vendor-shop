41da022d21b76f233435a629da605e98689f65e2
MohsenSami <mohsensami88@gmail.com>
2022-12-24 23:04:43 +0330

    frontend tweaks

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
