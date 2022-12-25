79b86fe3cc53d4087863b201981ba2b7ebfa7250
MohsenSami <mohsensami88@gmail.com>
2022-12-25 20:16:05 +0330

    vendor registration feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
-from .models import User
+from vendor.forms import VendorForm
+from .models import User, UserProfile
 from django.contrib import messages
 
 
@@ -27,4 +28,29 @@ def registerUser(request):
  
 
 def registerVendor(request):
-    return render(request, 'accounts/register-vendor.html')
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid():
+            password = form.cleaned_data['password']
+            user = form.save(commit=False)
+            user.set_password(password)
+            user.role = User.VENDOR
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            vendor.user_profile = UserProfile.objects.get(user=user)
+            vendor.save()
+            messages.success(request, 'Your account has been registered sucessfully! Please wait for the approval.')
+            return redirect('register-vendor')
+        else:
+            print('invalid form')
+            print(form.errors)
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+    context = {
+        'form': form,
+        'v_form': v_form
+    }
+    return render(request, 'accounts/register-vendor.html', context)
