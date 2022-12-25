29e6d63f59838a08491e19b6f1df64a1c92a8b5d
MohsenSami <mohsensami88@gmail.com>
2022-12-25 20:19:17 +0330

    vendor admin config

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Vendor
+from vendor.models import Vendor
 
 
-admin.site.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
+    list_display_links = ('user', 'vendor_name')
+    list_editable = ('is_approved',)
+
+admin.site.register(Vendor, VendorAdmin)
