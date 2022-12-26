dda0425bc2e376d43d517461e423b6be9692859f
MohsenSami <mohsensami88@gmail.com>
2022-12-26 18:27:04 +0330

    Send verification email

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from .forms import UserForm
 from vendor.forms import VendorForm
 from .models import User, UserProfile
 from django.contrib import messages, auth
-from .utils import detectUser
+from .utils import detectUser, send_verification_email
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 
@@ -36,6 +36,12 @@ def registerUser(request):
             user.set_password(password)
             user.role = User.CUSTOMER
             user.save()
+
+            # Send verification email
+            mail_subject = 'Please activate your account'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
+
             messages.success(request, 'Your account has been registered sucessfully!')
             return redirect('register-user')
         else:
@@ -66,6 +72,12 @@ def registerVendor(request):
             vendor.user = user
             vendor.user_profile = UserProfile.objects.get(user=user)
             vendor.save()
+
+            # Send verification email
+            mail_subject = 'Please activate your account'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
+            
             messages.success(request, 'Your account has been registered sucessfully! Please wait for the approval.')
             return redirect('register-vendor')
         else:
@@ -79,6 +91,11 @@ def registerVendor(request):
         'v_form': v_form
     }
     return render(request, 'accounts/register-vendor.html', context)
+
+
+def activate(request, uidb64, token):
+    # Activate the user by setting the is_active status to True
+    return
 
 
 def login(request):
--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-from ..env import *
+from .env import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,3 +146,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mohsensami88@gmail.com'
+DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <mohsensami88@gmail.com>'
