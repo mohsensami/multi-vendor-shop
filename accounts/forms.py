1337089470ea73aedbe3e3e18e85603a68df2b20
MohsenSami <mohsensami88@gmail.com>
2022-12-28 17:35:23 +0330

    Restaurant profile form setup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
 
 
 class UserForm(forms.ModelForm):
@@ -18,3 +18,11 @@ class UserForm(forms.ModelForm):
             raise forms.ValidationError(
                 "Password does not match!"
             )
+
+
+class UserProfileForm(forms.ModelForm):
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
--- vendor/views.py
+++ vendor/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from accounts.models import UserProfile
+from vendor.models import Vendor
+from .forms import VendorForm
+from accounts.forms import UserProfileForm
 
 
 def vprofile(request):
-    return render(request, 'vendor/vprofile.html')
+    profile = get_object_or_404(UserProfile, user=request.user)
+    vendor = get_object_or_404(Vendor, user=request.user)
+
+    profile_form = UserProfileForm(instance = profile)
+    vendor_form = VendorForm(instance=vendor)
+
+    context = {
+        'profile_form': profile_form,
+        'vendor_form': vendor_form,
+    }
+    return render(request, 'vendor/vprofile.html', context)
