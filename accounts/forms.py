e7c81d00b2fdc1d31d641e933c139577a20877a7
MohsenSami <mohsensami88@gmail.com>
2022-12-28 18:41:55 +0330

    Prepare address field for google geocoding

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,6 +22,7 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
 
