01d01279f8d9e024a8f07ecf3f42525055cd4050
MohsenSami <mohsensami88@gmail.com>
2022-12-30 11:34:11 +0330

    Menu builder fooditem in frontend

--- vendor/views.py
+++ vendor/views.py
@@ -8,7 +8,12 @@ from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required, user_passes_test
 from accounts.views import check_role_vendor
-from menu.models import Category
+from menu.models import Category, FoodItem
+
+
+def get_vendor(request):
+    vendor = Vendor.objects.get(user=request.user)
+    return vendor
 
 
 @login_required(login_url='login')
@@ -42,10 +47,25 @@ def vprofile(request):
     return render(request, 'vendor/vprofile.html', context)
 
 
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def menu_builder(request):
-    vendor = Vendor.objects.get(user=request.user)
+    vendor = get_vendor(request)
     categories = Category.objects.filter(vendor=vendor)
     context = {
         'categories': categories
     }
     return render(request, 'vendor/menu_builder.html', context)
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def fooditems_by_category(request, pk=None):
+    vendor = get_vendor(request)
+    category = get_object_or_404(Category, pk=pk)
+    food_items = FoodItem.objects.filter(vendor=vendor, category=category)
+    context = {
+        'fooditems': food_items,
+        'category': category
+    }
+    return render(request, 'vendor/fooditems_by_category.html', context)
