328d3277d83e424c2d13757e515a253c5aebe2c8
MohsenSami <mohsensami88@gmail.com>
2022-12-30 11:06:04 +0330

    Menu builder category in frontend

--- vendor/views.py
+++ vendor/views.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required, user_passes_test
 from accounts.views import check_role_vendor
+from menu.models import Category
 
 
 @login_required(login_url='login')
@@ -42,4 +43,9 @@ def vprofile(request):
 
 
 def menu_builder(request):
-    return render(request, 'vendor/menu_builder.html')
+    vendor = Vendor.objects.get(user=request.user)
+    categories = Category.objects.filter(vendor=vendor)
+    context = {
+        'categories': categories
+    }
+    return render(request, 'vendor/menu_builder.html', context)
