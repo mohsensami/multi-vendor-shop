c1a599a0571f7a6d574c7e94dcba8d3e777352f3
MohsenSami <mohsensami88@gmail.com>
2022-12-31 22:42:15 +0330

    Handle empty value errors

--- config/urls.py
+++ config/urls.py
@@ -24,5 +24,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
