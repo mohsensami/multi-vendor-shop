69aebb6bf2eb9764378283957172919105bca164
MohsenSami <mohsensami88@gmail.com>
2023-01-02 20:24:45 +0330

    Update the cart counter and item qty realtime

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
+from .context_processors import get_cart_counter
 from vendor.models import Vendor
 from menu.models import Category, FoodItem
 from django.db.models import Prefetch
@@ -48,10 +49,10 @@ def add_to_cart(request, food_id):
                     # Increase the cart quantity
                     chkCart.quantity += 1
                     chkCart.save()
-                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity'})
+                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
                 except:
                     chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
-                    return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart'})
+                    return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity})
             except:
                 return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
         else:
