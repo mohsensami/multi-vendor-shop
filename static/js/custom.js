a3e5e6946c11eeb746acf97173d9a12f6e8031a9
MohsenSami <mohsensami88@gmail.com>
2023-01-02 18:28:52 +0330

    Add to cart sending ajax request

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,6 +2,20 @@ $(document).ready(function(){
     // add to cart
     $('.add_to_cart').on('click', function(e){
         e.preventDefault();
-        alert('ajax add to cart');
+
+        food_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+
+        data = {
+            food_id: food_id
+        }
+        $.ajax({
+            type: 'GET',
+            url: url,
+            data: data,
+            success: function(response){
+                alert(response);
+            }
+        })
     })
 })
