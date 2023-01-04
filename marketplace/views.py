484ac8274808b4a7c98d1d734e2da0efbb260ce5
MohsenSami <mohsensami88@gmail.com>
2023-01-04 20:50:21 +0330

    is open or closed added in vendor market place details page

--- marketplace/views.py
+++ marketplace/views.py
@@ -6,7 +6,7 @@ from menu.models import Category, FoodItem
 from django.db.models import Prefetch
 from .models import Cart
 from django.contrib.auth.decorators import login_required
-from datetime import date
+from datetime import date, datetime
 
 
 def marketplace(request):
@@ -34,6 +34,18 @@ def vendor_detail(request, vendor_slug):
     today_date = date.today()
     today = today_date.isoweekday()
     current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day= today)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    is_open = None
+    for i in current_opening_hours:
+        start = str(datetime.strptime(i.from_hour, "%I:%M %p").time())
+        end = str(datetime.strptime(i.to_hour, "%I:%M %p").time())
+        if current_time > start and current_time < end:
+            is_open = True
+            break
+        else:
+            is_open = False
 
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
@@ -44,7 +56,8 @@ def vendor_detail(request, vendor_slug):
         'categories': categories,
         'cart_items': cart_items,
         'opening_hours': opening_hours,
-        'current_opening_hours': current_opening_hours
+        'current_opening_hours': current_opening_hours,
+        'is_open': is_open
     }
     return render(request, 'marketplace/vendor_detail.html',context)
 
